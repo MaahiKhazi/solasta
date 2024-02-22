9f0b217017fd9d45c92396f2c9be3fdebc2b1208
Maahi <mohammadmaahi7@gmail.com>
2024-02-22 16:55:54 +0530

    json obj done

--- try/proj/src/App.jsx
+++ try/proj/src/App.jsx
@@ -37,10 +37,16 @@ function App() {
 
           const result = await model.generateContent(prompt);
           const response = await result.response;
-          //const text = response.text();
-          //console.log(response.data)
-          // After fetching data stored it in posts state. 
-          setPosts(response.text); 
+          const data = JSON.stringify(response);
+          const jsonData = JSON.parse(data);
+          if (jsonData && jsonData.candidates && jsonData.candidates.length > 0) {
+            const paths = jsonData.candidates[0].content.parts[0].text;
+            const mpaths = paths.slice(3, -3)
+            // Now 'paths' contains the JSON string representing the paths
+            setPosts(mpaths);
+          } else {
+            console.error("Invalid JSON structure in the response");
+          }
 
           // Closed the loading page 
           setLoading(false); 
@@ -48,7 +54,7 @@ function App() {
 
       // Call the function 
       loadPost(); 
-  }, []); 
+  }, []);
   return (
     <div>
       <h1>This is generative AI </h1>
@@ -59,7 +65,7 @@ function App() {
             // posts((item) => ( 
             //  // Presently we only fetch 
             //  // title from the API 
-            <h4>{posts}</h4> 
+            <h4>{posts}</h4>
            // )) 
           )} 
       </div>
