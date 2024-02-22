4333cd238c22b1fcdd2c941b2bc211c2894f20a1
priyashanand <anandpriyash17@gmail.com>
2024-02-22 15:37:56 +0530

    sccessfully posted the json data as h1 tag

--- try/proj/src/App.jsx
+++ try/proj/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { GoogleGenerativeAI } from '@google/generative-ai'
 
 
@@ -6,6 +6,7 @@ const genAI = new GoogleGenerativeAI("AIzaSyDuNTuGXba6sBd5VjXxOG3ldEHrbDRf_L0");
 
 async function run() {
   // For text-only input, use the gemini-pro model
+  
   const model = genAI.getGenerativeModel({ model: "gemini-pro"});
 
   const prompt = 'Give me the JSON for an array object called "paths" containing 10 learning paths that suggest user for learning new programming skills which are related to what he knows. Paths should be in the form of objects which have title, description and skills (new or old) as keys. The user knows ReactJs, Node.js, JavaScript, Python, C, C++ language(s).'
@@ -18,11 +19,50 @@ async function run() {
 }
 
 function App() {
-  const result = run()
+
+  const [loading, setLoading] = useState(false); 
+  const [posts, setPosts] = useState([]); 
+  
+  useEffect(() => { 
+      const loadPost = async () => { 
+          // Till the data is fetch using API 
+          // the Loading page will show. 
+          setLoading(true); 
+
+          // Await make wait until that 
+          // promise settles and return its result 
+          const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+
+          const prompt = 'Give me the JSON for an array object called "paths" containing 10 learning paths that suggest user for learning new programming skills which are related to what he knows. Paths should be in the form of objects which have title, description and skills (new or old) as keys. The user knows ReactJs, Node.js, JavaScript, Python, C, C++ language(s).'
+
+          const result = await model.generateContent(prompt);
+          const response = await result.response;
+          //const text = response.text();
+          //console.log(response.data)
+          // After fetching data stored it in posts state. 
+          setPosts(response.text); 
+
+          // Closed the loading page 
+          setLoading(false); 
+      }; 
+
+      // Call the function 
+      loadPost(); 
+  }, []); 
   return (
     <div>
       <h1>This is generative AI </h1>
-      <p>{result}</p>
+      <div>
+        {loading ? ( 
+              <h4>Loading...</h4> 
+          ):( 
+            // posts((item) => ( 
+            //  // Presently we only fetch 
+            //  // title from the API 
+            <h4>{posts}</h4> 
+           // )) 
+          )} 
+      </div>
     </div>
   )
 }
